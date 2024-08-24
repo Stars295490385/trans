c530b295d27bade4bae749d363098b148e48ac3e
Teoh TeikToe <teohteiktoe@gmail.com>
2024-08-24 06:18:38 +0000

    v1

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-print("to replace http-server")
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
 
 
 
